450ed1f7b0a5c22afc054b85f2836f387291e7a5
bpoiss <bernhard.poiss@cyberhouse.at>
2016-01-15 14:05:45 +0100

    Initial commit

---
+++ Bpoiss/MyModule/Block/CustomBlock.php
@@ -0,0 +1,11 @@
+<?php
+namespace Bpoiss\MyModule\Block;
+use Magento\Framework\View\Element\Template;
+
+class CustomBlock extends Template
+{
+    protected function _prepareLayout()
+    {
+
+    }
+}
---
+++ Bpoiss/MyModule/Controller/Index/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bpoiss\MyModule\Controller\Index;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
